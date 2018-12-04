ba0be8d69b1d8aafe5d80e5aa5cb2543c9cb54bc
Tinker Tian <tinker.tian@hotmail.com>
2018-12-04 17:06:14 +0800

    spring boot plugin

--- snowflake-spring/src/main/java/org/tinker/snowflake/spring/SnowflakeComponent.java
+++ snowflake-spring/src/main/java/org/tinker/snowflake/spring/SnowflakeComponent.java
@@ -11,43 +11,70 @@ import javax.annotation.PostConstruct;
 
 @Component
 public class SnowflakeComponent {
-    private final static Object             largeSnowflakeLock = new Object();
-    private static       LargeSnowflake     largeSnowflake;
-    private final static Object             smallSnowflakeLock = new Object();
-    private static       SmallSnowflake     smallSnowflake;
-    private static       ApplicationContext context;
-    private static       Environment        environment;
+    private final static Object           largeSnowflakeLock = new Object();
+    private static       LargeSnowflake[] largeSnowflakeArray;
+    private final static Object           smallSnowflakeLock = new Object();
+    private static       SmallSnowflake[] smallSnowflakeArray;
+    private static       Environment      environment;
+    private static       int              largeStart;
+    private static       int              largeEnd;
+    private static       int              largeCurrentIndex;
+    private static       int              smallStart;
+    private static       int              smallEnd;
+    private static       int              smallCurrentIndex;
 
     @Autowired
     ApplicationContext applicationContext;
 
     @PostConstruct
     private void init() {
-        this.context = applicationContext;
         this.environment = applicationContext.getEnvironment();
+        this.largeStart = Integer.valueOf(environment.getProperty("snowflake.node.large").split("-")[0]);
+        this.largeEnd = Integer.valueOf(environment.getProperty("snowflake.node.large").split("-")[1]);
+        this.largeCurrentIndex = this.largeStart;
+        this.smallStart = Integer.valueOf(environment.getProperty("snowflake.node.small").split("-")[0]);
+        this.smallEnd = Integer.valueOf(environment.getProperty("snowflake.node.small").split("-")[1]);
+        this.smallCurrentIndex = this.smallStart;
     }
 
     public static long nextLarge() {
-        if (largeSnowflake == null) {
+        if (largeSnowflakeArray == null) {
             synchronized (largeSnowflakeLock) {
-                if (largeSnowflake == null) {
-                    largeSnowflake = new LargeSnowflake(Integer.valueOf(environment.getProperty("snowflake.node.large")));
+                if (largeSnowflakeArray == null) {
+                    largeSnowflakeArray = new LargeSnowflake[largeEnd - largeStart + 1];
+                    for (int i = largeStart; i <= largeEnd; i++) {
+                        largeSnowflakeArray[i] = new LargeSnowflake(i);
+                    }
                 }
             }
         }
-
-        return largeSnowflake.next();
+        long sid = largeSnowflakeArray[largeCurrentIndex].next();
+        if (largeCurrentIndex > largeEnd) {
+            largeCurrentIndex = largeStart;
+        } else {
+            largeCurrentIndex++;
+        }
+        return sid;
     }
 
     public static long nextSmall() {
-        if (smallSnowflake == null) {
+        if (smallSnowflakeArray == null) {
             synchronized (smallSnowflakeLock) {
-                if (smallSnowflake == null) {
-                    smallSnowflake = new SmallSnowflake(Integer.valueOf(environment.getProperty("snowflake.node.small")));
+                if (smallSnowflakeArray == null) {
+                    smallSnowflakeArray = new SmallSnowflake[smallEnd - smallStart + 1];
+                    for (int i = smallStart; i <= smallEnd; i++) {
+                        smallSnowflakeArray[i] = new SmallSnowflake(i);
+                    }
                 }
             }
         }
 
-        return smallSnowflake.next();
+        long sid = smallSnowflakeArray[smallCurrentIndex].next();
+        if (smallCurrentIndex > smallEnd) {
+            smallCurrentIndex = smallStart;
+        } else {
+            smallCurrentIndex++;
+        }
+        return sid;
     }
 }
