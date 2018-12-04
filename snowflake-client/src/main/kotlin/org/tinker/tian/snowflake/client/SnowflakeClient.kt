5e0b09501be0c60c35cc6ea963f8400664993c49
mt <tinker.tian@hotmail.com>
2018-12-04 19:42:40 +0800

    snowflake client

---
+++ snowflake-client/src/main/kotlin/org/tinker/tian/snowflake/client/SnowflakeClient.kt
@@ -0,0 +1,35 @@
+package org.tinker.tian.snowflake.client
+
+import jodd.http.HttpRequest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
+import org.springframework.core.env.Environment
+import org.springframework.stereotype.Component
+import org.tinker.snowflake.spring.SnowflakeDomain
+import javax.annotation.PostConstruct
+
+@Component
+class SnowflakeClient {
+    @Autowired
+    lateinit var applicationContext: ApplicationContext
+
+    @PostConstruct
+    private fun init() {
+        environment = applicationContext.environment
+    }
+
+    companion object {
+        lateinit var environment: Environment
+
+        fun next(): Long {
+            return HttpRequest.get("${environment.getProperty("snowflake.server.host")}:1010/sf/next-small")
+                    .send()
+                    .body()
+                    .toLong()
+        }
+
+        fun resolver(sid: Long): SnowflakeDomain {
+            return SnowflakeDomain.resolverLarge(sid)
+        }
+    }
+}
